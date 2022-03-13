ba13ecb8187c635b8fc7890039067ce749f6612e
Wiktor <wiktor8555@gmail.com>
2022-03-13 17:07:58 +0200

    Header

--- git.php
+++ git.php
@@ -1,5 +1,6 @@
 <?php
 	$username = 'witold1885';
-	$token = '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';
+	// $token = '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';
+	$token = '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';
 
 ?>
