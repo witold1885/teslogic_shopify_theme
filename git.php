34c031c46f6caadfcfd8d485922c2b871e36cba1
Wiktor <wiktor8555@gmail.com>
2022-03-14 14:01:41 +0200

    Header

--- git.php
+++ git.php
@@ -2,7 +2,8 @@
 	$username = 'witold1885';
 	// $token = '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';
 	// $token = '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';
-	$token = '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';
+	// $token = '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';
+	$token = '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';
 	/*
 		git add . && git commit -m "Header" && git push
 	*/
