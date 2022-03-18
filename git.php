c1700a55ddabac9e908a7bc749059d4d2f457dd4
Wiktor <wiktor8555@gmail.com>
2022-03-18 12:13:45 +0200

    Header

--- git.php
+++ git.php
@@ -3,7 +3,8 @@
 	// $token = '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';
 	// $token = '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';
 	// $token = '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';
-	$token = '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';
+	// $token = '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';
+	$token = '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';
 	/*
 		git add . && git commit -m "Header" && git push
 	*/
