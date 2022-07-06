3acd9097fe207c97c66efd2d07d79546afae267d
witold1885 <wiktor8555@gmail.com>
2022-07-06 15:37:04 +0300

    Fix main banner

--- git.php
+++ git.php
@@ -1,13 +1,6 @@
 <?php
 	$username = 'witold1885';
-	// $token = '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';
-	// $token = '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';
-	// $token = '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';
-	// $token = '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';
-	// $token = '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';
-	// $token = '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';
-	// $token = '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';
-	$token = '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';
+	$token = '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';
 	/*
 		git add . && git commit -m "Header" && git push
 	*/
