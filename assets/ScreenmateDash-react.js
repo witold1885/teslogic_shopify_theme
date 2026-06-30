42553269981fcebcf78819b2950c69cf312f0220
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-06-30 19:25:22 +0300

    React build: importmap snippets

--- assets/ScreenmateDash-react.js
+++ assets/ScreenmateDash-react.js
@@ -1,1 +1,1 @@
-import{n as e,t}from"./react-chunk-jsx-runtime-DIBGU2nq.js";import"./react-chunk-ProductLayout-DcZRhwgt.js";e(),t();
+import{n as e,t}from"./react-chunk-jsx-runtime-DIBGU2nq.js";import"./react-chunk-ProductLayout-DnXeIKmm.js";e(),t();
