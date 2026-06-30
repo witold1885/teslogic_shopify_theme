9773bb638da29689cd3e90db4a6e9d3cb06bfa44
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-06-30 15:45:02 +0300

    Temp remove large files

--- _react/src/assets/icons/screenmate-one/commands/index.ts
+++
@@ -1,6 +0,0 @@
-const iconModules: Record<string, any> = import.meta.glob('./*.svg', { eager: true })
-const iconMap: Record<string, any> = Object.entries(iconModules).reduce((acc, [key, mod]) => ({
-    ...acc,
-    [key.replace('./', '').replace('.svg', '')]: mod.default
-}), {})
-export default iconMap
