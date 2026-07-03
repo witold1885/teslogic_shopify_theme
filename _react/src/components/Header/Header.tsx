c68544d2c162d420c47f95e1583d9af1314a3675
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-03 17:01:37 +0300

    Header mobile

--- _react/src/components/Header/Header.tsx
+++ _react/src/components/Header/Header.tsx
@@ -1,52 +1,90 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import './styles.scss'
 import { useAppSelector } from '../../redux/hooks'
-import { useScroll } from '../../hooks/scroll'
+import type { MenuItem } from '../../types/shopify'
 import { Button, Icon, Image } from '../Common'
 import logo from '../../assets/images/logo.svg'
-import chevron from '../../assets/icons/chevron-down.svg'
+import logoBlack from '../../assets/images/logo-black.svg'
+import ChevronDownIcon from '../../assets/icons/ChevronDownIcon'
+import closeIcon from '@/assets/icons/close-black.svg'
 
-const Header: React.FC = () => {
-    const { main_menu } = useAppSelector(state => state.content)
+import { useInlineStyles } from '../../hooks/inline-styles'
+import { useScroll } from '../../hooks/scroll'
+
+interface HeaderMenuProps {
+    className?: string
+    menu: MenuItem[]
+    mode: 'desktop' | 'mobile'
+}
+
+const HeaderMenu: React.FC<HeaderMenuProps> = ({ className = '', menu, mode }) => {
     const [openMenuIndex, setOpenMenuIndex] = useState<number | null>(null)
-    const scrollOffset = useScroll()
 
     return (
-        <header className={`header ${scrollOffset.y > 0 ? 'sticky' : ''}`}>
-            <Image className="header-logo" src={logo} alt="Screenmate" />
-            <div className="header-menu flex">
-                {main_menu?.map(({ title, children }, index) => (
+        <div className="flex-end-center gap-28 mob:flex-column-between mob:flex-1 mob:gap-0">
+            <div {...{className}}>
+                {menu.map(({ title, children }, index) => (
                     <div
                         key={index}
                         className="header-menu-item"
-                        onMouseEnter={() => setOpenMenuIndex(index)}
-                        onMouseLeave={() => setOpenMenuIndex(null)}
+                        onMouseEnter={mode === 'desktop' ? () => setOpenMenuIndex(index) : () => {}}
+                        onMouseLeave={mode === 'desktop' ? () => setOpenMenuIndex(null) : () => {}}
+                        onClick={mode === 'mobile' ? () => setOpenMenuIndex(index) : () => {}}
                     >
-                        {index < main_menu.length - 1 ? (<>
-                            <span className="header-menu-item-title">{title}</span>
-                            {index === 0 && <span className="header-menu-item-new">NEW</span>}
-                            <div className="header-menu-item-chevron">
-                                <Icon className="flex-center" icon={chevron} />
-                            </div>
-                            {openMenuIndex === index && children?.length !== 0 && (
-                                <ul className="header-menu-item-dropdown">
-                                    {children?.map((child, i) => (
-                                        <li key={i}>
-                                            <a href={child.url}>{child.title}</a>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </>) : (
-                            <Button className="header-menu-item-button">
-                                <span>{title}</span>
-                            </Button>
+                        <span className="header-menu-item-title">{title}</span>
+                        {index === 0 && <span className="header-menu-item-new">NEW</span>}
+                        <div className="header-menu-item-chevron">
+                            <Icon className="flex-center" svg={<ChevronDownIcon color={mode === 'desktop' ? '#FFF' : '#000'} />} />
+                        </div>
+                        {openMenuIndex === index && children?.length !== 0 && (
+                            <ul className="header-menu-item-dropdown">
+                                {children?.map((child, i) => (
+                                    <li key={i}>
+                                        <a href={child.url}>{child.title}</a>
+                                    </li>
+                                ))}
+                            </ul>
                         )}
                     </div>
                 ))}
             </div>
-        </header>
+            <Button className="header-menu-button">
+                <span>ORDER NOW</span>
+            </Button>
+        </div>
     )
+}
+
+const Header: React.FC = () => {
+    const { isMobile } = useInlineStyles()
+    const scrollOffset = useScroll()
+
+    const [openMobileMenu, setOpenMobileMenu] = useState<boolean>(false)
+
+    const { main_menu } = useAppSelector(state => state.content)
+
+    const menu = useMemo(() => {
+        return main_menu ? main_menu.filter(({ title }) => title !== 'Buy Now') : []
+    }, [main_menu])
+
+    return (<>
+        <header className={`header ${scrollOffset.y > 0 ? 'sticky' : ''}`}>
+            <Image className="header-logo" src={logo} alt="Screenmate" />
+            {!isMobile 
+                ? <HeaderMenu className="header-menu-desktop" menu={menu as MenuItem[]} mode="desktop" />
+                : <Button className="header-menu-mobile-open" onClick={() => setOpenMobileMenu(true)}><span>Menu</span></Button>
+            }
+        </header>
+        {openMobileMenu && (
+            <div className="header-menu-mobile-wrap">
+                <div className="flex-between-center">
+                    <Image className="header-logo" src={logoBlack} alt="Screenmate" />
+                    <Icon className="header-menu-mobile-close" icon={closeIcon} onClick={() => setOpenMobileMenu(false)}/>
+                </div>
+                <HeaderMenu className="header-menu-mobile" menu={menu as MenuItem[]} mode="mobile" />
+            </div>
+        )}
+    </>)
 }
 
 export default Header
