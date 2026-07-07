786dfbc57cc456d2aa92db77d77b5e763753b108
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-07 17:14:49 +0300

    React: change videos

--- _react/src/assets/videos/screenmate-one/index.ts
+++ _react/src/assets/videos/screenmate-one/index.ts
@@ -1,13 +1,13 @@
 const externalUrls: Record<string, string> = {
-    bannerDesktop: 'https://cdn.shopify.com/videos/c/o/v/b4811410d37f47059d56d608e1f66d20.mp4',
-    beyondBasicControls: 'https://cdn.shopify.com/videos/c/o/v/c6262283d8db4a719d02ebb8bfa231ed.mp4',
-    carPlayAndAndroidAuto: 'https://cdn.shopify.com/videos/c/o/v/3c13d464625e4ac3a1e37fb87aacb8dc.mp4',
-    connectConsoles: 'https://cdn.shopify.com/videos/c/o/v/6f8a3a49e9bd4308b428e9b906ef0aa2.mp4',
-    dualViewMode: 'https://cdn.shopify.com/videos/c/o/v/161e918796f7495594e143a140398a50.mp4',
-    gaming: 'https://cdn.shopify.com/videos/c/o/v/7d4d21e3be90472791cf64a9ccc1a075.mp4',
-    navigation: 'https://cdn.shopify.com/videos/c/o/v/905e7e5d4eac4106ac554397ddc5f572.mp4',
-    social: 'https://cdn.shopify.com/videos/c/o/v/10108c9aa67e45a59bccaf814920a41d.mp4',
-    streaming: 'https://cdn.shopify.com/videos/c/o/v/3514ca64f7a84b37afc57b3a752b359d.mp4',
+    bannerDesktop: 'https://cdn.shopify.com/videos/c/o/v/e41a762f70c045989e8e3b8a114058a7.mp4',
+    beyondBasicControls: 'https://cdn.shopify.com/videos/c/o/v/abba7f7a20c14f008ef949bbfa313d7f.mp4',
+    carPlayAndAndroidAuto: 'https://cdn.shopify.com/videos/c/o/v/78427390f7d44055b9436e760a222545.mp4',
+    connectConsoles: 'https://cdn.shopify.com/videos/c/o/v/32805c45d2404c08b619e4a2d1d97629.mp4',
+    dualViewMode: 'https://cdn.shopify.com/videos/c/o/v/4d115270240c4785a23ab9dc3ef26d8a.mp4',
+    gaming: 'https://cdn.shopify.com/videos/c/o/v/d39835a4d40b49668f428b463362587c.mp4',
+    navigation: 'https://cdn.shopify.com/videos/c/o/v/05f39950af5547b093ab2d995c754ac7.mp4',
+    social: 'https://cdn.shopify.com/videos/c/o/v/03c0dfbba36543b19799ec6289123f29.mp4',
+    streaming: 'https://cdn.shopify.com/videos/c/o/v/246dc0a4d9dc45839ff77cc3f0d67763.mp4',
 }
 
 const isProd: boolean = import.meta.env.PROD
