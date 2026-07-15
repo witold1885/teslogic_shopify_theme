9b7e4ed72d4d68220af383a540acca76394f6f70
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-15 20:11:42 +0300

    React: build fix

--- _react/src/main.tsx
+++ _react/src/main.tsx
@@ -8,6 +8,8 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import App from './App.tsx'
 
+console.log('TEST')
+
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
