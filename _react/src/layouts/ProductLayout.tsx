7c86c452f22297d892400111d47930d6c5d78419
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-25 11:48:14 +0300

    React: scroll to top on page reload

--- _react/src/layouts/ProductLayout.tsx
+++ _react/src/layouts/ProductLayout.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, type ReactNode } from 'react'
+import React, { lazy, Suspense, useEffect, type ReactNode } from 'react'
 import '@/assets/styles/common.scss'
 import Header from '../components/Header/Header'
 import LazySection from './LazySection'
@@ -12,21 +12,31 @@ interface ProductLayoutProps {
     children?: ReactNode
 }
 
-const ProductLayout: React.FC<ProductLayoutProps> = ({ className, onOrder, children }) => (<>
-    <Header onOrder={onOrder} />
-    <div {...{className}}>
-        {children}
+const ProductLayout: React.FC<ProductLayoutProps> = ({ className, onOrder, children }) => {    
+    useEffect(() => {
+        if (window.history && 'scrollRestoration' in window.history) {
+            window.history.scrollRestoration = 'manual'
+        }
+
+        window.scrollTo(0, 0)
+    }, [])
+
+    return (<>
+        <Header onOrder={onOrder} />
+        <div {...{className}}>
+            {children}
+            <LazySection>
+                <Suspense>
+                    <Reviews />
+                </Suspense>
+            </LazySection>
+        </div>
         <LazySection>
             <Suspense>
-                <Reviews />
+                <Footer />
             </Suspense>
         </LazySection>
-    </div>
-    <LazySection>
-        <Suspense>
-            <Footer />
-        </Suspense>
-    </LazySection>
-</>)
+    </>)
+}
 
 export default ProductLayout
