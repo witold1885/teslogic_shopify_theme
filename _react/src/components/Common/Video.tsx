b4a0a22ecf7e4a87b7796575ea1946bba435c9e0
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-28 16:23:11 +0300

    React: video component some fixes

--- _react/src/components/Common/Video.tsx
+++ _react/src/components/Common/Video.tsx
@@ -141,7 +141,7 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
                     // visibility: isReadyToPlay ? 'visible' : 'hidden'
                     opacity: isLoaded ? 1 : 0
                 }}
-                // src={finalSrc}
+                src={finalSrc}
                 loop={loop}
                 muted
                 {...{ fetchpriority, muted: true } as React.HTMLAttributes<HTMLVideoElement>}
@@ -162,9 +162,7 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
                 // preload={isIntersecting && isActive ? 'auto' : 'none'}
                 // onCanPlayThrough={() => setIsReadyToPlay(true)}
                 // onLoadedData={() => setIsReadyToPlay(true)}
-            >
-                <source type="video/mp4" src={finalSrc} />
-            </video>
+            />
         </div>
     )
 })
