b55c96ab6f46dafd448bd2d0cee7f02708d63e1b
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-28 14:47:02 +0300

    React: video component some fixes

--- _react/src/components/Common/Video.tsx
+++ _react/src/components/Common/Video.tsx
@@ -46,6 +46,20 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
         return () => observer.disconnect()
     }, [])
 
+    const safePlay = () => {
+        const video = videoRef.current
+        if (!video) return
+
+        const playPromise = video.play()
+        if (playPromise !== undefined) {
+            playPromise.then(() => setIsLoaded(true)).catch((error) => {
+                if (error.name !== 'AbortError') {
+                    console.error('Video play error:', error)
+                }
+            })
+        }
+    }
+
     useEffect(() => {
         const video = videoRef.current
         if (!video) return
@@ -54,47 +68,54 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
         const shouldPlay = isIntersecting && isActive
 
         if (shouldPlay) {
-            if (!video.src || video.src !== finalSrc) {
-                video.src = finalSrc
-            }
+            // if (!video.src || video.src !== finalSrc) {
+            //     video.src = finalSrc
+            // }
             // video.src = finalSrc
             // video.load()
             
             if (autoPlay) {
-                const playPromise = video.play()
-                if (playPromise !== undefined) {
-                    playPromise.then(() => setIsLoaded(true)).catch((error) => {
-                        if (error.name !== 'AbortError') {
-                            console.error('Autoplay failed:', error)
-                        }
-                    })
+                // const playPromise = video.play()
+                // if (playPromise !== undefined) {
+                //     playPromise.then(() => setIsLoaded(true)).catch((error) => {
+                //         if (error.name !== 'AbortError') {
+                //             console.error('Autoplay failed:', error)
+                //         }
+                //     })
+                // }
+                if (video.readyState >= 1) {
+                    safePlay()
                 }
             }
         } else {
             video.pause()
             // video.currentTime = 0
-            video.removeAttribute('src')
-            video.load() 
-            // setIsReadyToPlay(false)
-            setIsLoaded(false)
+            try {
+                video.currentTime = 0
+            } catch (e) {}
+            // video.removeAttribute('src')
+            // video.load() 
+            // // setIsReadyToPlay(false)
+            // setIsLoaded(false)
         }
     }, [isIntersecting, isActive, finalSrc, autoPlay, isReadyToPlay])
 
     useImperativeHandle(ref, () => ({
         play: () => {
-            if (videoRef.current) {
-                if (!videoRef.current.src) {
-                    videoRef.current.src = finalSrc
-                }
-                const playPromise = videoRef.current.play()
-                if (playPromise !== undefined) {
-                    playPromise.then(() => setIsLoaded(true)).catch((error) => {
-                        if (error.name !== 'AbortError') {
-                            console.error('Manual play failed:', error)
-                        }
-                    })
-                }
-            }
+            // if (videoRef.current) {
+            //     if (!videoRef.current.src) {
+            //         videoRef.current.src = finalSrc
+            //     }
+            //     const playPromise = videoRef.current.play()
+            //     if (playPromise !== undefined) {
+            //         playPromise.then(() => setIsLoaded(true)).catch((error) => {
+            //             if (error.name !== 'AbortError') {
+            //                 console.error('Manual play failed:', error)
+            //             }
+            //         })
+            //     }
+            // }
+            safePlay()
         },
         pause: () => {
             if (videoRef.current) {
@@ -120,14 +141,20 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
                     // visibility: isReadyToPlay ? 'visible' : 'hidden'
                     opacity: isLoaded ? 1 : 0
                 }}
-                // src={finalSrc}
+                src={finalSrc}
                 loop={loop}
                 {...{ fetchpriority } as React.HTMLAttributes<HTMLVideoElement>}
                 playsInline
                 // @ts-ignore
                 webkit-playsinline="true"
                 muted
-                preload="none"
+                preload="metadata"
+                onLoadedMetadata={() => {
+                    if (isIntersecting && autoPlay) {
+                        safePlay()
+                    }
+                }}
+                onPlaying={() => setIsLoaded(true)}
                 // preload={isIntersecting && isActive ? 'auto' : 'none'}
                 // onCanPlayThrough={() => setIsReadyToPlay(true)}
                 // onLoadedData={() => setIsReadyToPlay(true)}
