5bd53aa50587d1a6e92ed2e5651f95dd6b25b0b7
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-07-28 16:07:07 +0300

    React: video component some fixes

--- _react/src/components/Common/Video.tsx
+++ _react/src/components/Common/Video.tsx
@@ -68,54 +68,54 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
         const shouldPlay = isIntersecting && isActive
 
         if (shouldPlay) {
-            if (!video.src || video.src !== finalSrc) {
-                video.src = finalSrc
-            }
+            // if (!video.src || video.src !== finalSrc) {
+            //     video.src = finalSrc
+            // }
             // video.src = finalSrc
             // video.load()
             
             if (autoPlay) {
-                const playPromise = video.play()
-                if (playPromise !== undefined) {
-                    playPromise.then(() => setIsLoaded(true)).catch((error) => {
-                        if (error.name !== 'AbortError') {
-                            console.error('Autoplay failed:', error)
-                        }
-                    })
-                }
+                // const playPromise = video.play()
+                // if (playPromise !== undefined) {
+                //     playPromise.then(() => setIsLoaded(true)).catch((error) => {
+                //         if (error.name !== 'AbortError') {
+                //             console.error('Autoplay failed:', error)
+                //         }
+                //     })
+                // }
                 // if (video.readyState >= 1) {
-                    // safePlay()
+                    safePlay()
                 // }
             }
         } else {
             video.pause()
             // video.currentTime = 0
-            // try {
-            //     video.currentTime = 0
-            // } catch (e) {}
-            video.removeAttribute('src')
-            video.load() 
+            try {
+                video.currentTime = 0
+            } catch (e) {}
+            // video.removeAttribute('src')
+            // video.load() 
             // // setIsReadyToPlay(false)
-            setIsLoaded(false)
+            // setIsLoaded(false)
         }
     }, [isIntersecting, isActive, finalSrc, autoPlay, isReadyToPlay])
 
     useImperativeHandle(ref, () => ({
         play: () => {
-            if (videoRef.current) {
-                if (!videoRef.current.src) {
-                    videoRef.current.src = finalSrc
-                }
-                const playPromise = videoRef.current.play()
-                if (playPromise !== undefined) {
-                    playPromise.then(() => setIsLoaded(true)).catch((error) => {
-                        if (error.name !== 'AbortError') {
-                            console.error('Manual play failed:', error)
-                        }
-                    })
-                }
-            }
-            // safePlay()
+            // if (videoRef.current) {
+            //     if (!videoRef.current.src) {
+            //         videoRef.current.src = finalSrc
+            //     }
+            //     const playPromise = videoRef.current.play()
+            //     if (playPromise !== undefined) {
+            //         playPromise.then(() => setIsLoaded(true)).catch((error) => {
+            //             if (error.name !== 'AbortError') {
+            //                 console.error('Manual play failed:', error)
+            //             }
+            //         })
+            //     }
+            // }
+            safePlay()
         },
         pause: () => {
             if (videoRef.current) {
@@ -141,7 +141,7 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
                     // visibility: isReadyToPlay ? 'visible' : 'hidden'
                     opacity: isLoaded ? 1 : 0
                 }}
-                src={finalSrc}
+                // src={finalSrc}
                 loop={loop}
                 muted
                 {...{ fetchpriority, muted: true } as React.HTMLAttributes<HTMLVideoElement>}
@@ -153,17 +153,17 @@ const Video = forwardRef<VideoRefMethods, VideoProps>(({ className = '', style, 
                 // @ts-ignore
                 webkit-playsinline="true"
                 preload="metadata"
-                // onLoadedMetadata={() => {
-                //     if (isIntersecting && autoPlay) {
-                //         safePlay()
-                //     }
-                // }}
-                // onPlaying={() => setIsLoaded(true)}
+                onLoadedMetadata={() => {
+                    if (isIntersecting && autoPlay) {
+                        safePlay()
+                    }
+                }}
+                onPlaying={() => setIsLoaded(true)}
                 // preload={isIntersecting && isActive ? 'auto' : 'none'}
                 // onCanPlayThrough={() => setIsReadyToPlay(true)}
                 // onLoadedData={() => setIsReadyToPlay(true)}
             >
-                {/* <source type="video/mp4" src={finalSrc} /> */}
+                <source type="video/mp4" src={finalSrc} />
             </video>
         </div>
     )
