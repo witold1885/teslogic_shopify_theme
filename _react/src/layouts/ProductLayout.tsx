15e899df1fc20ed2f2a8451b9d40be9f51668bce
witold1885 <101505930+witold1885@users.noreply.github.com>
2026-09-08 23:37:49 +0300

    React: enable cookies banner css

--- _react/src/layouts/ProductLayout.tsx
+++ _react/src/layouts/ProductLayout.tsx
@@ -1,6 +1,6 @@
 import React, { lazy, useEffect, type ReactNode } from 'react'
 import '@/assets/styles/common.scss'
-// import '@/assets/styles/cookies-banner.scss'
+import '@/assets/styles/cookies-banner.scss'
 // import { EXTERNAL_SCRIPTS, useExternalScripts } from '../hooks/external-scripts'
 import Header from '../components/Header/Header'
 import LazySection from './LazySection'
